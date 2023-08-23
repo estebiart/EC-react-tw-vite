edd3880369e3939f6a5a4d4659de14d53a7956fb
Anderson Lozada <andersonlozada@quadi.co>
2023-08-22 22:35:50 -0500

    feat: :art: first structure

---
+++ react-course/src/pages/App/index.jsx
@@ -0,0 +1,19 @@
+import Home from '../Home'
+import MyAccount from '../MyAccount'
+
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-100'>
+        <Home />
+        <MyAccount/>
+      </div>
+    </>
+  )
+}
+
+export default App

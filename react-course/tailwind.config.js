51d5d886d13b6e0375754b26d3031c71b31de8c9
Anderson Lozada <andersonlozada@quadi.co>
2023-08-22 22:29:37 -0500

    feat: :zap: initial structure

---
+++ react-course/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'wave-pattern': "url('../img/wave-white.png')",
+        'wave-pattern-dots': "url('../img/pattern-white-dots.png')",
+        'pattern-characters-red': "url('../img/pattern-characters-red.png')",
+        'character-mario': "url('../img/character-l.png')",
+        'wave-pink': "url('../img/wave-pink.png')",
+        'yellow-dots': "url('../img/pattern-yellow-dots.png')",
+      }),
+      screens:{
+        'xsm': '320px'
+
+      }
+    }
+  },
+  plugins: [],
+}
